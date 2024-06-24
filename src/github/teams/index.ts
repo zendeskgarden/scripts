655279bcf4fe05adbabdabd3edae4928741ab444
Jonathan Zempel <jzempel@gmail.com>
2024-06-24 14:25:25 -0400

    Handle `github-teams` user queries

--- src/github/teams/index.ts
+++ src/github/teams/index.ts
@@ -5,6 +5,7 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+import { GraphQlQueryResponseData, graphql } from '@octokit/graphql';
 import { repository as getRepository, token as getToken } from '../index.js';
 import { handleErrorMessage, handleSuccessMessage } from '../../utils/index.js';
 import { Command } from 'commander';
@@ -23,25 +24,50 @@ export const execute = async (args: IGitHubTeamsArgs): Promise<string[] | undefi
   let retVal: string[] | undefined;
 
   try {
+    const auth = args.token || (await getToken(args.spinner));
+    const github = new Octokit({ auth });
     const org = (args.org || (await getRepository(args.path, args.spinner))?.owner)!;
 
     if (args.user) {
-      // {
-      //   organization(login: "my-org") {
-      //     teams(userLogins: ["johndoe"]) {
-      //       totalCount
-      //       edges {
-      //         node {
-      //           name
-      //         }
-      //       }
-      //     }
-      //   }
-      // }
-    } else {
-      const auth = args.token || (await getToken(args.spinner));
-      const github = new Octokit({ auth });
+      let userLogin;
 
+      if (args.user === true) {
+        /* https://docs.github.com/en/rest/users/users#get-the-authenticated-user */
+        const currentUser = await github.users.getAuthenticated();
+
+        userLogin = currentUser.data.login;
+      } else {
+        userLogin = args.user;
+      }
+
+      /* https://docs.github.com/en/graphql/reference/objects#teamconnection */
+      const userTeams: GraphQlQueryResponseData = await graphql(
+        `
+          query userTeams($login: String!, $userLogin: String!) {
+            organization(login: $login) {
+              teams(first: 100, userLogins: [$userLogin]) {
+                edges {
+                  node {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        `,
+        {
+          login: org,
+          userLogin,
+          headers: {
+            authorization: `token ${auth}`
+          }
+        }
+      );
+
+      retVal = userTeams.organization.teams.edges
+        .map((edge: { node: { name: string } }) => edge.node.name)
+        .sort();
+    } else {
       /* https://octokit.github.io/rest.js/v17#teams-list */
       const teams = await github.teams.list({ org });
 
