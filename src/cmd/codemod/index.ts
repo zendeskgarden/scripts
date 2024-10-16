d21503937aee5d14daaacfae0cbcb66f15376d3b
Jonathan Zempel <jzempel@gmail.com>
2024-10-16 17:58:12 -0400

    Improve execution results

--- src/cmd/codemod/index.ts
+++ src/cmd/codemod/index.ts
@@ -5,10 +5,17 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import { ExecaError, execa } from 'execa';
 import { handleErrorMessage, handleSuccessMessage } from '../../utils/index.js';
 import { Command } from 'commander';
 import { Ora } from 'ora';
+import { execa } from 'execa';
+
+type RETVAL = {
+  errored: number;
+  unmodified: number;
+  skipped: number;
+  modified: number;
+};
 
 interface ICommandCodemodArgs {
   paths: string[] | string;
@@ -22,8 +29,8 @@ interface ICommandCodemodArgs {
  * @param {string[] | string} args.paths Code path globs.
  * @param {string} args.transform Location of the transform file.
  */
-export const execute = async (args: ICommandCodemodArgs): Promise<string | undefined> => {
-  let retVal: string | undefined;
+export const execute = async (args: ICommandCodemodArgs): Promise<RETVAL | undefined> => {
+  let retVal: RETVAL | undefined;
 
   try {
     const paths = Array.isArray(args.paths) ? args.paths : [args.paths];
@@ -35,9 +42,24 @@ export const execute = async (args: ICommandCodemodArgs): Promise<string | undef
     ];
     const codemod = await execa('jscodeshift', codemodArgs, { preferLocal: true });
 
-    retVal = codemod.stdout;
+    if (codemod.stderr) {
+      throw new Error(codemod.stderr);
+    } else {
+      const errored = /^(?<errored>\d+) errors$/gmu.exec(codemod.stdout)?.groups?.errored;
+      const unmodified = /^(?<unmodified>\d+) unmodified$/gmu.exec(codemod.stdout)?.groups
+        ?.unmodified;
+      const skipped = /^(?<skipped>\d+) skipped$/gmu.exec(codemod.stdout)?.groups?.skipped;
+      const modified = /^(?<modified>\d+) ok$/gmu.exec(codemod.stdout)?.groups?.modified;
+
+      retVal = {
+        errored: errored ? parseInt(errored, 10) : 0,
+        unmodified: unmodified ? parseInt(unmodified, 10) : 0,
+        skipped: skipped ? parseInt(skipped, 10) : 0,
+        modified: modified ? parseInt(modified, 10) : 0
+      };
+    }
   } catch (error: unknown) {
-    handleErrorMessage((error as ExecaError).message, 'cmd-codemod', args.spinner);
+    handleErrorMessage((error as Error).message, 'cmd-codemod', args.spinner);
 
     throw error;
   }
@@ -55,15 +77,19 @@ export default (spinner: Ora): Command => {
       '-t --transform <transform>',
       'location (local path or remote URL) of the transform file'
     )
+    .option('--pretty', 'pretty-print result')
     .action(async (paths: string[]) => {
       try {
         spinner.start();
 
         const options = command.opts();
-        const codemod = await execute({ paths, transform: options.transform, spinner });
+        const result = await execute({ paths, transform: options.transform, spinner });
 
-        if (codemod) {
-          handleSuccessMessage(codemod, spinner);
+        if (result) {
+          const space = options.pretty ? 2 : undefined;
+          const message = JSON.stringify(result, undefined, space);
+
+          handleSuccessMessage(message, spinner);
         } else {
           throw new Error();
         }
